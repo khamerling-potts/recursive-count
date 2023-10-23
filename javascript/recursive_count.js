c01fe8436fef4fd0cc2357fdea905614051b12ef
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-23 10:02:24 -0400

    recursive count done

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,9 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num === 10) {
+    return;
+  }
+  console.log(num);
+  recursiveCount(num + 1);
 }
 
 if (require.main === module) {
@@ -11,3 +15,8 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+base case: if count equals 10, return
+print count each time
+call recursiveCount(count+1)
+*/
